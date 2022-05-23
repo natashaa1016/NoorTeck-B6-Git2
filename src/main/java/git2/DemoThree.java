76d46222fb3516f4ce24f92ffa385bbc15327d09
Natas <Natas@DESKTOP-0A525AG.fios-router.home>
2022-05-23 18:26:29 -0400

    natasha

---
+++ src/main/java/git2/DemoThree.java
@@ -0,0 +1,17 @@
+package git2;
+
+public class DemoThree {
+
+	public static void main(String[] args) {
+	
+			System.out.println("Demo Three...");
+			
+
+	}
+
+	public void subtract(int a ,int b) {
+	int result = a/b;
+	System.out.println(result);
+}
+}
+//natasha
--- src/main/java/git2/DemoTwo.java
+++ src/main/java/git2/DemoTwo.java
@@ -5,6 +5,21 @@ public class DemoTwo {
 	public static void main(String[] args) {
 		 System.out.println("Git demo 2...");
 
+		 
 	}
 
+	public static void main(String[]args) {
+		System.out.println("git demo 3...");
+	}
+	
+	
+	public static void main (String[]args) {
+		System.out.println("git demo 4..");
+	}
+	
+	
+	
+	
 }
+	
+
